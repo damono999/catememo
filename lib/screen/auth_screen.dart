36d4f2cc57baf3f60576191717131e5115099f7c
because-engineer <kancan877@gmail.com>
2021-05-01 19:36:49 +0900

    add: ログインの監視 #3

---
+++ lib/screen/auth_screen.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginPage extends StatelessWidget {
+  static final googleLogin = GoogleSignIn(scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("カテメモ"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            GoogleSignInAccount signinAccount = await googleLogin.signIn();
+            if (signinAccount == null) return;
+            GoogleSignInAuthentication auth =
+                await signinAccount.authentication;
+            final GoogleAuthCredential credential =
+                GoogleAuthProvider.credential(
+              idToken: auth.idToken,
+              accessToken: auth.accessToken,
+            );
+            await FirebaseAuth.instance.signInWithCredential(credential);
+          },
+          child: Text(
+            'login',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
