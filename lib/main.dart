ad68752fe3437401b24707287acd37acba6b52f8
because-engineer <kancan877@gmail.com>
2021-05-01 17:24:52 +0900

    add: ログイン機能 #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -12,51 +17,89 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'カテメモ'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class LoginPage extends StatelessWidget {
+  static final googleLogin = GoogleSignIn(scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ]);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("カテメモ"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            GoogleSignInAccount signinAccount = await googleLogin.signIn();
+            if (signinAccount == null) return;
+            GoogleSignInAuthentication auth =
+                await signinAccount.authentication;
+            final GoogleAuthCredential credential =
+                GoogleAuthProvider.credential(
+              idToken: auth.idToken,
+              accessToken: auth.accessToken,
+            );
+            User user =
+                (await FirebaseAuth.instance.signInWithCredential(credential))
+                    .user;
+            if (user != null) {
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return LogoutPage(user);
+                }),
+              );
+            }
+          },
+          child: Text(
+            'login',
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LogoutPage extends StatelessWidget {
+  User user;
+  LogoutPage(this.user);
+  static final googleLogin = GoogleSignIn(scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text("Hello ${user.displayName}", style: TextStyle(fontSize: 50)),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () async {
+                FirebaseAuth.instance.signOut();
+                await googleLogin.signIn();
+                await Navigator.of(context)
+                    .pushReplacement(MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }));
+              },
+              child: Text('Logout', style: TextStyle(fontSize: 50)),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
